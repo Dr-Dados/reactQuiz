759084f79e55d2f2b8babafa973fd78b029f292e
Dr.Dados <adnane.bachchar@gmail.com>
2024-10-31 12:19:53 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from "../assets/quiz-logo.png";
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="logo app" />
+      <h1></h1>
+    </header>
+  );
+};
+
+export default Header;
